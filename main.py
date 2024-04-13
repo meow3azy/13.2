0d6be74c34d9904d8d0bee9ca031eb20b315b51e
Pavel Isaev <killroy1297@gmail.com>
2024-04-13 16:29:29 +0300

    new

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
 class Category:
-    def __init__(self, name, products):
-        self.name = name
-        self.products = products
+    def __init__(self, name):
+        self.__name = name
+        self.__products = []
 
     def add_product(self, product):
-        self.products.append(product)
+        self.__products.append(product)
 
     @property
-    def товары(self):
-        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.products]
+    def products(self):
+        return [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
 
     def __str__(self):
-        return f"{self.name}, количество продуктов: {len(self.products)} шт."
-
+        return f"{self.__name}, количество продуктов: {len(self.__products)} шт."
 
 
 class Product:
     def __init__(self, name, price, quantity):
         self.name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @property
-    def цена(self):
-        return self.price
+    def price(self):
+        return self.__price
 
-    @цена.setter
-    def цена(self, new_price):
+    @price.setter
+    def price(self, new_price):
         if new_price <= 0:
             print("Цена введена некорректно")
-        elif new_price < self.price:
+        elif new_price < self.__price:
             confirm = input("Цена товара понижается. Вы уверены? (y/n): ")
             if confirm.lower() == "y":
-                self.price = new_price
+                self.__price = new_price
                 print("Цена товара успешно изменена")
             else:
                 print("Действие отменено")
         else:
-            self.price = new_price
+            self.__price = new_price
+
+    @staticmethod
+    def create_product(name, price, quantity):
+        return Product(name, price, quantity)
 
     def __str__(self):
-        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
-
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
